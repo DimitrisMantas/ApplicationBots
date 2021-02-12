c962583d8066f426c1651719d5bfd76a00891230
Dimitris Mantas <75796651+DimitrisMantas@users.noreply.github.com>
2021-02-12 04:00:55 +0200

    Update MagicTiles3.py

--- MagicTiles3.py
+++ MagicTiles3.py
@@ -18,20 +18,20 @@ import win32con
 # pyautogui.displayMousePosition()
 
 
-# Set the colors on which the left mouse button will be clicked and released.
+# Register the colors on which the left mouse button will be clicked and released.
 LEFT_MOUSE_HOLD_ON_RGB_VALUES = [0, 0, 0]
 LEFT_MOUSE_RELEASE_ON_RGB_VALUES = [0, 150, 225]
 
 
-# Set the precision for comparing two RGB values to each other.
-PRECISION = 75
+# Register the precision for comparing two RGB values to each other.
+PRECISION = 112.5
 
 
-# Set the programm runtime interupt button.
+# Register the programm runtime interupt button.
 KEYBOARD_INTERRUPT = "C"
 
 
-# Set the screen position of the midpoint of each column.
+# Register the screen position of the midpoint of each column.
 # The Y-coordinates need to be set near the horizontal white line at the bottom, but not on it.
 COLUMN_MIDPOINTS_X_CORRDINATES = [720, 860, 1000, 1140]
 COLUMN_MIDPOINTS_Y_CORRDINATES = 540
@@ -39,24 +39,38 @@ COLUMN_MIDPOINTS = [COLUMN_MIDPOINTS_X_CORRDINATES[0], COLUMN_MIDPOINTS_Y_CORRDI
 
 
 def get_rgb_values(x, y):
-    """Get the RBG values of the pixels, which are located some screen position."""
+    """Get the RBG values of the px., which are located some screen position."""
     return pyautogui.pixel(x, y)
 
 
-def check_rgb_match(sample_rgb, target_rgb, precision):
+def check_rgb_match(sample_rgb, target_rgb, precision, mode):
     """Check if some sample RGB values is the same or "close enough" to some target RGB values."""
-    if abs(sum(sample_rgb) - sum(target_rgb)) <= precision:
-        return True
+    # Register the function return flag.
+    return_flag = False
+
+    if mode == "rgb":
+        if abs(sum(sample_rgb) - sum(target_rgb)) <= precision:
+            return_flag = True
+    elif mode == "r":
+        if abs(sample_rgb[0] - target_rgb[0]) <= precision:
+            return_flag = True
+    elif mode == "g":
+        if abs(sample_rgb[1] - target_rgb[1]) <= precision:
+            return_flag = True
+    elif mode == "b":
+        if abs(sample_rgb[2] - target_rgb[2]) <= precision:
+            return_flag = True
     else:
-        return False
+        return_flag = False
+    return return_flag
 
 
 def left_mouse_hold(x, y):
     """Register and hold a left mouse click at some screen position."""
-    # Set the mouse cursor screen position.
+    # Register the mouse cursor screen position.
     win32api.SetCursorPos((x, y))
 
-    # The next mouse event needs to be generated dx = dy = 0 from the current cursor position.
+    # The next mouse event needs to be generated dx = dy = 0 px. from the current cursor position.
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
 
 
@@ -71,14 +85,19 @@ def main(column_midpoints, left_mouse_hold_on_rgb_values, left_mouse_release_on_
     while not keyboard.is_pressed(keyboard_interrupt):
 
         # This line is for debugging purposes.
-        start_measuring_runtime = time.time()
+        # start_measuring_runtime = time.time()
+
+        # Sometimes the next mouse event needs to be generated dy > 0 px. from the current cursor position in order to account for the song speed.
+        dy = 75
+        # There is some inherent lag between some pair conesecutive mouse events.
+        ddy = 10
 
         # PyAutoGUI can throw an OSError for some reason. This block is for debugging purposes.
         try:
-            column_1 = check_rgb_match(get_rgb_values(column_midpoints[0], column_midpoints[1]), left_mouse_hold_on_rgb_values, precision)
-            column_2 = check_rgb_match(get_rgb_values(column_midpoints[2], column_midpoints[3]), left_mouse_hold_on_rgb_values, precision)
-            column_3 = check_rgb_match(get_rgb_values(column_midpoints[4], column_midpoints[5]), left_mouse_hold_on_rgb_values, precision)
-            column_4 = check_rgb_match(get_rgb_values(column_midpoints[6], column_midpoints[7]), left_mouse_hold_on_rgb_values, precision)
+            column_1 = check_rgb_match(get_rgb_values(column_midpoints[0], column_midpoints[1]), left_mouse_hold_on_rgb_values, precision, mode="b")
+            column_2 = check_rgb_match(get_rgb_values(column_midpoints[2], column_midpoints[3]), left_mouse_hold_on_rgb_values, precision, mode="b")
+            column_3 = check_rgb_match(get_rgb_values(column_midpoints[4], column_midpoints[5]), left_mouse_hold_on_rgb_values, precision, mode="b")
+            column_4 = check_rgb_match(get_rgb_values(column_midpoints[6], column_midpoints[7]), left_mouse_hold_on_rgb_values, precision, mode="b")
 
             column_12 = column_1 and column_2
             column_13 = column_1 and column_3
@@ -88,53 +107,56 @@ def main(column_midpoints, left_mouse_hold_on_rgb_values, left_mouse_release_on_
             column_34 = column_3 and column_4
 
             # Case 1: The column doesn't have a gradient.
-            if column_1 and not column_2 and not column_3 and not column_4:
-                left_mouse_hold(column_midpoints[0], column_midpoints[1])
-                left_mouse_release()
-            elif column_2 and not column_3 and not column_4 and not column_1:
-                left_mouse_hold(column_midpoints[2], column_midpoints[3])
-                left_mouse_release()
-            elif column_3 and not column_4 and not column_1 and not column_2:
-                left_mouse_hold(column_midpoints[4], column_midpoints[5])
-                left_mouse_release()
+            if column_1:
+                if column_12:
+                    left_mouse_hold(column_midpoints[0], column_midpoints[1] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[2], column_midpoints[3] + dy + ddy)
+                    left_mouse_release()
+                elif column_13:
+                    left_mouse_hold(column_midpoints[0], column_midpoints[1] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[4], column_midpoints[5] + dy + ddy)
+                    left_mouse_release()
+                elif column_14:
+                    left_mouse_hold(column_midpoints[0], column_midpoints[1] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[6], column_midpoints[7] + dy + ddy)
+                    left_mouse_release()
+                elif column_1 and not column_2 and not column_3 and not column_4:
+                    left_mouse_hold(column_midpoints[0], column_midpoints[1] + dy)
+                    left_mouse_release()
+            elif column_2:
+                if column_23:
+                    left_mouse_hold(column_midpoints[2], column_midpoints[3] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[4], column_midpoints[5] + dy + ddy)
+                    left_mouse_release()
+                elif column_24:
+                    left_mouse_hold(column_midpoints[2], column_midpoints[3] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[6], column_midpoints[7] + dy + ddy)
+                    left_mouse_release()
+                elif column_2 and not column_3 and not column_4 and not column_1:
+                    left_mouse_hold(column_midpoints[2], column_midpoints[3] + dy)
+                    left_mouse_release()
+            elif column_3:
+                if column_34:
+                    left_mouse_hold(column_midpoints[4], column_midpoints[5] + dy)
+                    left_mouse_release()
+
+                    left_mouse_hold(column_midpoints[6], column_midpoints[7] + dy + ddy)
+                    left_mouse_release()
+                elif column_3 and not column_4 and not column_1 and not column_2:
+                    left_mouse_hold(column_midpoints[4], column_midpoints[5] + dy)
+                    left_mouse_release()
             elif column_4 and not column_1 and not column_2 and not column_3:
-                left_mouse_hold(column_midpoints[6], column_midpoints[7])
-                left_mouse_release()
-            elif column_12:
-                left_mouse_hold(column_midpoints[0], column_midpoints[1])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[2], column_midpoints[3])
-                left_mouse_release()
-            elif column_13:
-                left_mouse_hold(column_midpoints[0], column_midpoints[1])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[4], column_midpoints[5])
-                left_mouse_release()
-            elif column_14:
-                left_mouse_hold(column_midpoints[0], column_midpoints[1])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[6], column_midpoints[7])
-                left_mouse_release()
-            elif column_23:
-                left_mouse_hold(column_midpoints[2], column_midpoints[3])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[4], column_midpoints[5])
-                left_mouse_release()
-            elif column_24:
-                left_mouse_hold(column_midpoints[2], column_midpoints[3])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[6], column_midpoints[7])
-                left_mouse_release()
-            elif column_34:
-                left_mouse_hold(column_midpoints[4], column_midpoints[5])
-                left_mouse_release()
-
-                left_mouse_hold(column_midpoints[6], column_midpoints[7])
+                left_mouse_hold(column_midpoints[6], column_midpoints[7] + dy)
                 left_mouse_release()
         except OSError:
             pass
