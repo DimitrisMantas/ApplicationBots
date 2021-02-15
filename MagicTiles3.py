646f1f6b6fb44b76521b3b2bb53997bca16e12d2
Dimitris Mantas <75796651+DimitrisMantas@users.noreply.github.com>
2021-02-15 12:44:50 +0200

    Update MagicTiles3.py

--- MagicTiles3.py
+++ MagicTiles3.py
@@ -18,41 +18,30 @@ import win32con
 # pyautogui.displayMousePosition()
 
 
-# Register the colors on which the left mouse button will be clicked and released.
-LEFT_MOUSE_HOLD_ON_RGB_VALUES = [0, 0, 0]
+# Register the colors on which a mouse event will be generated.
+LEFT_MOUSE_CLICK_ON_RGB_VALUES = [0, 0, 0]
 
 
 # Register the precision for comparing two RGB values to each other.
-PRECISION = 100
+PRECISION = 100 # Out of 3 * 255 = 765.
 
 
 # Register the programm runtime interupt button.
 KEYBOARD_INTERRUPT = "C"
 
 
-# Register the screen position of the midpoint of each column.
-# The Y-coordinates need to be set near the horizontal white line at the bottom, but not on it.
-COLUMN_MIDPOINTS_X_CORRDINATES = [720, 860, 1000, 1140]
-COLUMN_MIDPOINTS_Y_CORRDINATES = 540
-
-COLUMN_MIDPOINTS = [COLUMN_MIDPOINTS_X_CORRDINATES[0], COLUMN_MIDPOINTS_Y_CORRDINATES, COLUMN_MIDPOINTS_X_CORRDINATES[1], COLUMN_MIDPOINTS_Y_CORRDINATES, COLUMN_MIDPOINTS_X_CORRDINATES[2], COLUMN_MIDPOINTS_Y_CORRDINATES, COLUMN_MIDPOINTS_X_CORRDINATES[3], COLUMN_MIDPOINTS_Y_CORRDINATES]
-
-
-def get_rgb_values(x, y):
-    """Get the RBG values of the px., which are located some screen position."""
-    return pyautogui.pixel(x, y)
+# Register the screen position of the midpoint of each column in pixels.
+COLUMN_MIDPOINTS = [720, 540, 860, 540, 1000, 540, 1140, 540] # [Column 1, Column 2, Column 3, Column 4]
 
 
 def check_rgb_match(sample_rgb, target_rgb, precision):
     """Check if some sample RGB values is the same or "close enough" to some target RGB values."""
-    # Register the function return flag.
     return abs(sum(sample_rgb) - sum(target_rgb)) <= precision
 
 
 def left_mouse_click(x, y):
     """Register and hold a left mouse click at some screen position."""
-
-    # I'm gonna do what's called a pro-gamer move...
+    # Register a guard clause, because we don't want to register a mouse event when the value of x is equal is False.
     if not x: return
 
     # Register the mouse cursor screen position.
@@ -63,7 +52,7 @@ def left_mouse_click(x, y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 
-def main(column_midpoints, left_mouse_hold_on_rgb_values, precision, keyboard_interrupt):
+def main(column_midpoints, left_mouse_click_on_rgb_values, precision, keyboard_interrupt):
     """This is the entry point of the program."""
     # Sometimes the next mouse event needs to be generated dy > 0 px. from the current cursor position in order to account for the song speed.
     dy = 100
@@ -74,10 +63,10 @@ def main(column_midpoints, left_mouse_hold_on_rgb_values, precision, keyboard_in
 
         # PyAutoGUI can throw an OSError for some reason. This block is for debugging purposes.
         try:
-            left_mouse_click(column_midpoints[0] * check_rgb_match(get_rgb_values(column_midpoints[0], column_midpoints[1]), left_mouse_hold_on_rgb_values, precision), column_midpoints[1] + dy)
-            left_mouse_click(column_midpoints[2] * check_rgb_match(get_rgb_values(column_midpoints[2], column_midpoints[3]), left_mouse_hold_on_rgb_values, precision), column_midpoints[3] + dy)
-            left_mouse_click(column_midpoints[4] * check_rgb_match(get_rgb_values(column_midpoints[4], column_midpoints[5]), left_mouse_hold_on_rgb_values, precision), column_midpoints[5] + dy)
-            left_mouse_click(column_midpoints[6] * check_rgb_match(get_rgb_values(column_midpoints[6], column_midpoints[7]), left_mouse_hold_on_rgb_values, precision), column_midpoints[7] + dy)
+            left_mouse_click(column_midpoints[0] * check_rgb_match(pyautogui.pixel(column_midpoints[0], column_midpoints[1]), left_mouse_click_on_rgb_values, precision), column_midpoints[1] + dy)
+            left_mouse_click(column_midpoints[2] * check_rgb_match(pyautogui.pixel(column_midpoints[2], column_midpoints[3]), left_mouse_click_on_rgb_values, precision), column_midpoints[3] + dy)
+            left_mouse_click(column_midpoints[4] * check_rgb_match(pyautogui.pixel(column_midpoints[4], column_midpoints[5]), left_mouse_click_on_rgb_values, precision), column_midpoints[5] + dy)
+            left_mouse_click(column_midpoints[6] * check_rgb_match(pyautogui.pixel(column_midpoints[6], column_midpoints[7]), left_mouse_click_on_rgb_values, precision), column_midpoints[7] + dy)
         except OSError:
             pass
 
@@ -91,4 +80,4 @@ if __name__ == "__main__":
     # This helps bring the game screen out of the background.
     time.sleep(3)
 
-    main(COLUMN_MIDPOINTS, LEFT_MOUSE_HOLD_ON_RGB_VALUES, PRECISION, KEYBOARD_INTERRUPT)
+    main(COLUMN_MIDPOINTS, LEFT_MOUSE_CLICK_ON_RGB_VALUES, PRECISION, KEYBOARD_INTERRUPT)
