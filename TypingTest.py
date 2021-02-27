f85b6aec19622e4a566e705e50256f80846ae37b
Dimitris Mantas <75796651+DimitrisMantas@users.noreply.github.com>
2021-02-27 13:09:07 +0200

    Add files via upload

--- TypingTest.py
+++ TypingTest.py
@@ -1,4 +1,4 @@
-"""This is a basic bot for most online CPM and WPM measurement tools."""
+"""This is a basic bot for most online typing speed tests, as well as standalone CPM and WPM measurment tools."""
 """Copyright 2021 Dimitris Mantas"""
 
 
@@ -13,23 +13,24 @@ from PIL import ImageOps
 import pytesseract
 
 
-# This program requires Tesseract, an open-source OCR program, which was developed by Hewlett-Packard and is now maintained by Google [1].
+# This program requires Tesseract, an open-source OCR engine, which was originally developed by Hewlett-Packard and is now maintained by Google [1].
 
 # INSTALLATION INSTRUCTIONS
 
-# 1. Install the latest stable version of Tesseract [2].
+# 1. Install the latest version of Tesseract for Microsoft Windows [2].
 # 2. Install the Tesseract wrapper for Python [3].
-# 3. 
+# 3. Note the location of the Tesseract excecutable file.
 
 # [1]: https://en.wikipedia.org/wiki/Tesseract_(software)
 # [2]: https://github.com/UB-Mannheim/tesseract/wiki
 # [3]: https://pypi.org/project/pytesseract/
 
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" # Copy and paste the location of the Tesseract excecutable file here.
+# Copy and paste the location of the Tesseract excecutable file here.
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
-# This is a hyperlink to the very first measurement tool, which shows up on Google. (26/02/2021)
+# This is a hyperlink to the very first typing test, which shows up on Google. (26/02/2021)
 # https://www.typingtest.com/
 
 
@@ -38,90 +39,75 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 # pyautogui.displayMousePosition()
 
 
-# Register the dimensions of a bounding boxm which surely contains a single line of text.
-BOUNDING_BOX = (1150, 320, 1800, 355) # (Top Left Corner, Bottom Right Corner)
+# Register the coordinates of the maximum area bounding box, which contains the very first single line of text.
+FIRST_BOUNDING_BOX = (1150, 320, 1800, 355) # (Top Left Corner, Bottom Right Corner)
 
-# Register the vertical distance between two consecutive new lines of text in pixels.
-BOUNDING_BOX_OFFSET = 100
+# Register the vertical screen distance between two consecutive new lines of text.
+DY = 100
 
 
-BOUNDING_BOXES = {1: BOUNDING_BOX,
-                  2: (BOUNDING_BOX[0], BOUNDING_BOX[1] + BOUNDING_BOX_OFFSET, BOUNDING_BOX[2], BOUNDING_BOX[3] + BOUNDING_BOX_OFFSET)}
+# Register the bounding boxes for the first, as well as the second lines of text as a tuple of said tuples.
+BOUNDING_BOXES = (FIRST_BOUNDING_BOX,(FIRST_BOUNDING_BOX[0], FIRST_BOUNDING_BOX[1] + DY, FIRST_BOUNDING_BOX[2], FIRST_BOUNDING_BOX[3] + DY))
 
 
 # Register a program runtime interrupt key, so that the the infinite loop below can be stopped.
 KEYBOARD_INTERRUPT = "C"
 
 
-def grab_screenshot(bounding_box):
-    # Grab a screenshot of a specific area of the screen and store it in memory as a PIL image.
-    return ImageGrab.grab(bbox=bounding_box)
-
-
 def ocr(image):
-    """Perform OCR on a NumPy array, which encodes a B&W PIL image."""
-    # Any image manipulation must implemented below this line.
+    """Perform OCR on a NumPy array, which encodes a PIL image."""
+    # Any image manipulation must performed below this line. Color profile assignment is meant to be performed outside this function block.
 
 
-    ocr_string = pytesseract.image_to_string(image)
+    string = pytesseract.image_to_string(image)
 
-    return ocr_string[:-2] + " " # The last value in the OCR string is a special return character, which must be ignored.
+    # The last character in the resulting OCR string is a special return character, which must be ignored.
+    # Also, a single space must be registered at the end of each word.
+    return string[:-2] + " " 
 
 
-def write_ocr_string(ocr_string):
-    """ """
-    for i in range(len(ocr_string)):
-
-        if ocr_string[i] == " ":
+def write(string):
+    """Write a string of characters by simulating the keyboard."""
+    for i in range(len(string)):
+        # Spaces between consecutive characters are normally ignored.
+        if string[i] == " ":
             keyboard.press_and_release("space")
         else:
-            keyboard.write(ocr_string[i])
+            keyboard.write(string[i])
 
 
-def main(bounding_boxes, keyboard_interrupt):
+def main(bounding_box):
     """This is the entry point of the program."""
-    #
-    i = 1
+    # Grab the required screenshot, convert its color mode from RGB to B&W and finally encode it into a NumPy array.
+    screenshot = ImageOps.grayscale(ImageGrab.grab(bbox=bounding_box))
 
-    while not keyboard.is_pressed(keyboard_interrupt):
-        # This line is for debugging purposes.
-        # Start measuring the program runtime.
-        # runtime = time.time()
+    # This line is meant for debugging purposes only.
+    # Print said screenshot to a temporary PNG image.
+    # screenshot.show()
 
-        # The very first bounding box placed at its original location, while every second is placed dy down.
-        if i == 1:
-            bounding_box = bounding_boxes[1]
-        else:
-            # Wait for the next line of text tp be placed at the correct location.
-            time.sleep(0.25)
-
-            bounding_box = bounding_boxes[2]
-
-
-        # Grab the required screenshot and reset its color mode from RGB to B&W.
-        screenshot = ImageOps.grayscale(grab_screenshot(bounding_box))
-        # This line is meant for debugging purposes only.
-        # Display said screenshot as a PNG image.
-        # screenshot.show()
-
-        # Encode the corresponding PIL image data into a NumPy array.
-        screenshot = np.array(screenshot)
-
-        ocr_string = ocr(screenshot)
-
-        write_ocr_string(ocr_string)
-
-        i += 1
-        
-        # These lines are for debugging purposes.
-        # Compute the runtime of each loop.
-        # runtime = time.time() - runtime
-
-        # print("This loop took {0} seconds. The application is being run at {1} FPS.".format(runtime, runtime ** -1))
+    # Encode the required screenshot into a NumPy array, perform OCR on it and write the resulting string of characters.
+    write(ocr(np.array(screenshot)))
 
 
 if __name__ == "__main__":
     # Allow some buffer time in order to bring the game screen out of the current background.
     time.sleep(3)
 
-    main(BOUNDING_BOXES, KEYBOARD_INTERRUPT)
+    # The very first bounding box placed must be placed where it should, while every other one must be placed DY below. 
+    main(BOUNDING_BOXES[0])
+
+    while not keyboard.is_pressed(KEYBOARD_INTERRUPT):
+        # This line is for debugging purposes.
+        # Start measuring the program runtime.
+        # runtime = time.time()
+
+        # Wait for the next line of text to be placed where it should.
+        time.sleep(0.25)
+
+        main(BOUNDING_BOXES[1])
+
+        # These lines are for debugging purposes.
+        # Compute the runtime of each loop.
+        # runtime = time.time() - runtime
+
+        # print("This loop took {0} seconds. The application is being run at {1} FPS.".format(runtime, runtime ** -1))
